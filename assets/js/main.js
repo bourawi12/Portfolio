afba50d57e0245fb835b89215d43d1a1ff006750
bourawi12 <mohamed12bouaziz@gmail.com>
2024-01-18 15:14:24 +0100

    about me

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,12 @@ const linkAction =()=>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SHADOW HEADER ===============*/
-
+const scrollHeader=() => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll',scrollHeader)
 
 /*=============== EMAIL JS ===============*/
 
